9e70539ead928f729f4f5ee7718f7edc47ca2940
Joel <hilliardjoel05@gmail.com>
2025-08-17 14:58:34 -0700

    fix drag n dorp, remove bloat, remove disco theme

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,11 @@ import path from "path"
 import preact from "@preact/preset-vite"
 import { defineConfig } from "vite"
 
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig({
   plugins: [preact()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  define: {
-    __WS_TOKEN__: JSON.stringify(process.env.WS_TOKEN || ''),
-    'process.env.NODE_ENV': JSON.stringify(mode),
-  },
-  server: {
-    hmr: command === 'serve' ? {
-      clientPort: process.env.HMR_PORT ? parseInt(process.env.HMR_PORT) : undefined,
-    } : false,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-}))
+})
